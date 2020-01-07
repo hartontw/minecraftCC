13865dd0ffe6c484e62008f163b82192a8727763
Harton <hartontw@gmail.com>
2020-01-07 14:49:35 +0100

    Encryption api first version

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ A set of programs and tools for [ComputerCraft](http://www.computercraft.info/) 
 ## TODO
 1. An installer for work remotelly
 2. Graphic turtle OS
+
+## USEFUL REPOS
+- [dan200/ComputerCraft](https://github.com/dan200/ComputerCraft)
+- [SquidDev-CC/CC-Tweaked](https://github.com/SquidDev-CC/CC-Tweaked)
+- [CCEmuX/CCEmuX](https://github.com/CCEmuX/CCEmuX)
+- [SquidDev-CC/copy-cat](https://github.com/SquidDev-CC/copy-cat)
+- [SquidDev-CC/cloud-catcher](https://github.com/SquidDev-CC/cloud-catcher)
+- [oeed/CraftOS-Standards](https://github.com/oeed/CraftOS-Standards)
+- [eric-wieser/computercraft-github](https://github.com/eric-wieser/computercraft-github)
+- [somesocks/lua-lockbox](https://github.com/somesocks/lua-lockbox)
+- [benanders/LuaIDE](https://github.com/benanders/LuaIDE)
+- [lyqyd/cc-packman](https://github.com/lyqyd/cc-packman)
+- [lyqyd/ComputerCraft-LyqydNet](https://github.com/lyqyd/ComputerCraft-LyqydNet)
+- [SquidDev-CC/mbs](https://github.com/SquidDev-CC/mbs)
---
+++ apis/crypt.lua
@@ -0,0 +1,61 @@
+function RC4(key, text)
+    -- RC4
+    -- @zhuangzebo
+    -- https://github.com/zebozhuang/RC4
+    
+    local KSA = function(key)
+        local key_len = string.len(key)
+        local S = {}
+        local key_byte = {}
+    
+        for i = 0, 255 do
+            S[i] = i
+        end
+    
+        for i = 1, key_len do
+            key_byte[i-1] = string.byte(key, i, i)
+        end
+    
+        local j = 0
+        for i = 0, 255 do
+            j = (j + S[i] + key_byte[i % key_len]) % 256
+            S[i], S[j] = S[j], S[i]
+        end
+        return S
+    end
+    
+    local PRGA = function(S, text_len)
+        local i = 0
+        local j = 0
+        local K = {}
+    
+        for n = 1, text_len do
+    
+            i = (i + 1) % 256
+            j = (j + S[i]) % 256
+    
+            S[i], S[j] = S[j], S[i]
+            K[n] = S[(S[i] + S[j]) % 256]
+        end
+        return K
+    end
+    
+    local output = function(S, text)
+        local len = string.len(text)
+        local c = nil
+        local res = {}
+        for i = 1, len do
+            c = string.byte(text, i, i)
+            res[i] = string.char(bit.bxor(S[i], c))
+        end
+        return table.concat(res)
+    end
+    
+    return output(PRGA(KSA(key), string.len(text)), text)
+end
+
+function AES(key, text)
+    -- Lua AES Encryption
+    -- @lolmaker2002
+    -- https://pastebin.com/bQMnGjzE
+end
