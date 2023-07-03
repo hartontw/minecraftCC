ffec368a7c25cec5661947a627f5307e470adac1
Harton <hartontw@gmail.com>
2023-07-03 22:56:32 +0200

    Fixed requires

--- repository/locale/locale.lua
+++ repository/locale/locale.lua
@@ -5,10 +5,10 @@ return {
     getMessages = function(program_name)
         local path = settings.get("paths.messages")
         local lang = settings.getDetails("locale.lang")
-        local messages = require(path..program_name.."/"..lang.default..".lua")
+        local messages = require(path..program_name.."/"..lang.default)
         if lang.default ~= lang.value then
             if fs.exists(path..program_name.."/"..lang.value..".lua") then
-                local translate = require(path..program_name.."/"..lang.value..".lua")
+                local translate = require(path..program_name.."/"..lang.value)
                 for k, v in pairs(translate) do
                     messages[k] = v
                 end
